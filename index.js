a2d709e71471cc8d199d84c349c3a5b68f322dbf
Mabel Atombo <mabelatombo@gmail.com>
2024-07-21 00:46:26 +0000

    commits styling changes

---
+++ index.js
@@ -0,0 +1,34 @@
+alert ( "Hi my name is Mabel" ); 
+let name = prompt( "What is your name?" );
+alert("Hi " + name +",welcome to our website.");
+alert("How do you do?");
+let repponds= prompt("How do you do?");
+alert("Very well" + ", Thank you.");
+let title = prompt ("What is your title");
+let name = prompt ( "What is your name");
+let product =  prompt( "What is your favorite product?");
+let hometown= prompt ( "What is your hometown?");
+alert(`Hi, ${title}  ${name} who likes ${product}, and comes from ${ hometown} your are welcome to our website.`);
+// let name1 = "My name is Mabel"
+// let sentence = "hi there " + name1
+// console.log (name1);
+// console.log ( sentence);
+// let sentence1 = "Hello there 🙋‍♀️🙋‍♀️"
+// let name = "my name is Lily"
+// console.log(`${sentence1} , ${name}`);
+// let num = 5
+// console.log(num);
+// let num1 = 10
+// console.log(num1);
+// console.log(`${num}${num1}`);
+// let name = prompt( "What is your name?");
+// if(name === "Mabel"){
+//     console.log(name);
+//     alert("Hi Beautiful 🙋‍♀️");
+// } else{
+//     console.log( name);
+//     alert("Hi there!");
+// }
+// let name = prompt("What is your name?");
+// let age = prompt( "what is your age?");
+// let email = prompt("What is you email address?")
