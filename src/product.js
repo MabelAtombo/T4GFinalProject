464747dd8ee7e7c0b706cdbb1377ef81e5aa060f
Mabel Atombo <mabelatombo@gmail.com>
2024-07-29 11:42:05 +0000

    commits styling changes

---
+++ src/product.js
@@ -0,0 +1,9 @@
+function heading(){
+    let head= document.getElementById( "head");
+    head.style.fontFamily= "times new roman";
+    head.style.innerHTML=" A True Taste of Africa";
+    head.style.animation= "uppercase ";
+  }
+   let high= document.getElementById( "head");
+   high. addEventListener( "onclick", heading);
+   
