659063e2d507c9609a1e4d4fc5bb92f98b8d05c0
Mabel Atombo <mabelatombo@gmail.com>
2024-07-29 21:05:28 +0000

    commit all changes

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-function userDetails(){
-  let name = prompt ("What is your name?");
-  let age = prompt ("What is your age?");
-  let email = prompt (" Enter your email.");
-alert(`Hi🙋‍♀️,${name}who is ${age} with ${email} you are welcome to my homepag.Please get in touch.`);
-}
-console
+// function userDetails(){
+//   let name = prompt( "What is your name?");
+//   let age = prompt ("What is your age?");
+//   let email = prompt (" Enter your email.");
+// alert(`Hi🙋‍♀️,${name}who is ${age} with ${email} you are welcome to my homepag.Please get in touch.`);
+// }
 
+   //when the user enters the explore button on the homepage
+   //it takes their name 
+   function changeColor(){
+    let name = prompt( "What is your name?");
+    let head= document.getElementById("head");
+    head.style.overscrollBehavior= "hover";
+    head.innerHTML `🙋‍♀️ Hi ${name}, welcome to African Choice`;
+    let browse=document.getElementById(browse)
+    browse.innerHTML`Welcome ${name}. Taste The Raw Goodness Of Africa`;
+
+   }
+   let.linking=document.getElementById("linking");
+   linking.addEventListener("mouse4nter",changeColor);               
--- src/product.js
+++ src/product.js
@@ -1,9 +1,8 @@
-function heading(){
-    let head= document.getElementById( "head");
-    head.style.fontFamily= "times new roman";
-    head.style.innerHTML=" A True Taste of Africa";
-    head.style.animation= "uppercase ";
-  }
-   let high= document.getElementById( "head");
-   high. addEventListener( "onclick", heading);
-   
+// function heading(){
+//     let head= document.getElementById( "head");
+//     head.style.fontFamily= "times new roman";
+//     head.style.innerHTML=" A True Taste of Africa";
+//     head.style.animation= "uppercase ";
+//   }
+//    let high= document.getElementById( "high");
+//    high. addEventListener( "onclick", heading);
